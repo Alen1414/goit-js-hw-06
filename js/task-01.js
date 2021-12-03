87a48cd3c5b422f24e8dbeb11a5a741af17d16a5
Alona Puhach <alen1414@gmail.com>
2021-12-03 13:01:09 -0400

    number one

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,39 @@
+// Напиши скрипт который:
 
-// document.querySelector('ul');
-const ul = document.querySelectorAll('li.item')
-// document.getElementById('categories');
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
 
-console.dir(document)
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+const numberEL = document.querySelectorAll('li.item');
+console.log(' Number of categories:',numberEL.length);
+
+const categoryEL = document.querySelector('h2');
+console.log('Category:', categoryEL.textContent);
+
+const elementsEL = document.querySelectorAll('ul');
+console.log('Elements:', elementsEL.length);
+
+
+
+
+const productsEL = document.querySelectorAll('h2');
+
+console.log('Category:', productsEL[1].textContent);
+console.log('Elements:', productsEL.length);
+
+console.log('Category:', productsEL[2].textContent);
+
+const elementsProdEL = document.querySelectorAll('ul');
+console.log('Elements:',elementsProdEL.length)
